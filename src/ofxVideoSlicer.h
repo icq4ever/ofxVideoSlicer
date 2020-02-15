b42cf07b2bfc16c990aaebe9d57f7ee55f3bfee3
Yi donghoon <icq4ever@gmail.com>
2020-02-15 17:56:06 +0900

    function description added

--- src/ofxVideoSlicer.h
+++ src/ofxVideoSlicer.h
@@ -56,6 +56,10 @@ public:
 		jpgOn = false;
     }
     
+	//--------------------------------------------------------------
+	// set save Directory
+	// if not called, save directory where original file located
+	//--------------------------------------------------------------
 	void setTargetFolder(string directoryName){
 		saveDirectory = directoryName + "/";
 	}
